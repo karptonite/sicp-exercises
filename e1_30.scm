6d8e9e01006881f165e06d02c5795f9ce69567e1
Daniel Karp <danielkarp@gmail.com>
2016-02-13 11:09:26 -0500

    Exercise 1.30

---
+++ e1_30.scm
@@ -0,0 +1,10 @@
+(define (inc n) (+ n 2))
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+      result
+      (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(define (cube n) (* n n n))
+(define (identity x) x)

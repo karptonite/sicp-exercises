2899a32379d3249e52ba88c91aad52f625e30410
Daniel Karp <danielkarp@gmail.com>
2016-02-28 13:26:29 -0500

    Exercises 38-39

--- e1_37.scm
+++ e1_37.scm
@@ -11,12 +11,28 @@
            12)
 
 (define (cont-frac-rec n d k)
-  (define (cont-frac-helper n d k count)
-    (if (= k count)
-      (/ (n k) (d k))
-      (/ (n k) (+ (d k) (cont-frac-helper n d k (+ 1 count))))))
-  (cont-frac-helper n d k 0))
+  (define (cont-frac-helper n d k i)
+    (if (= k i)
+      (/ (n i) (d i))
+      (/ (n i) (+ (d i) (cont-frac-helper n d k (+ 1 i))))))
+  (cont-frac-helper n d k 1))
 
 (cont-frac-rec (lambda (i) 1.0)
            (lambda (i) 1.0)
            12)
+
+(define e-minus-2 
+  (cont-frac-rec (lambda (i) 1.0)
+             (lambda (i)
+               (if (= (remainder (+ i 1) 3) 0) 
+                 (* 2.0 (/ (+ i 1.0) 3.0))
+                 1.0))
+             50))
+
+(define (tan-cf x k)
+  (cont-frac-rec (lambda (i)
+               (if (= i 1) x
+                 (- (* x x))))
+             (lambda (i) (- (* i 2.0) 1))
+             k))
+

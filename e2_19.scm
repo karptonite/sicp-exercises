1ce398c5df6d7b84493c1ed6720fbcbef905bbb6
Daniel Karp <danielkarp@gmail.com>
2016-04-03 11:16:56 -0400

    Exercise 2.19

---
+++ e2_19.scm
@@ -0,0 +1,21 @@
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+          (+ (cc amount
+                 (except-first-denomination coin-values))
+             (cc (- amount
+                    (first-denomintation coin-values))
+                 coin-values)))))
+
+(define (no-more? coin-values)
+  (null? coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (first-denomintation coin-values)
+  (car coin-values))
+
+(define us-coins (list 25 10 5 1 50))
+
